86495581d827065a39ef52503ed2c712b029f5eb
Jay Allen <allen.jeremiah555@gmail.com>
2018-01-28 13:12:35 -0500

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { multiple, poppins, smokyBlackToGigas, smokyBlack, mayaBlue, gigas } from './styled/vars';
+import { multiple, poppins, jet, mayaBlue, gigas, gigasToMayaBlue } from './styled/vars';
 
 const Div = styled.div`
-  background: ${smokyBlackToGigas};
+  background: ${gigasToMayaBlue};
   color: #fff;
-  border-bottom: 3px solid ${gigas};
-  padding: 2rem;
+  padding: 1.6rem 3rem;
   box-shadow: 0 1px 4px ${mayaBlue};
 
   h1 {
-    font-size: 4rem;
+    font-size: 3.5rem;
     font-family: ${poppins};
     text-shadow: ${multiple};
   }
--- src/components/styled/vars.js
+++ src/components/styled/vars.js
@@ -1,14 +1,14 @@
 // Colors vars
-const grey = '#465362';
-const black = '#2b303a';
-const purple = '#b47eb3';
-const white = '#edfffc';
-const red = '#d64933';
-const blue = '#3D5A80';
-const lightBlue = '#98C1D9'; 
-const orange = '#fe7f2d';
-const redOrange = '#ec4e20';
-const waxFlower = '#fec0aa';
+// const grey = '#465362';
+// const black = '#2b303a';
+// const purple = '#b47eb3';
+// const white = '#edfffc';
+// const red = '#d64933';
+// const blue = '#3D5A80';
+// const lightBlue = '#98C1D9'; 
+// const orange = '#fe7f2d';
+// const redOrange = '#ec4e20';
+// const waxFlower = '#fec0aa';
 
 export const pastelYellow = '#f8fa90'; 
 export const jet = '#363732';
@@ -24,9 +24,9 @@ export const lato = "Lato', sans-serif";
 export const poppins = "'Poppins', sans-serif;";
 
 //gradients
-export const smokyBlackToGigas = `linear-gradient(to right,${smokyBlack}, ${gigas})`;
+export const gigasToMayaBlue = `linear-gradient(to right, ${gigas}, ${mayaBlue})`;
 
 //text shadow
-export const multiple = `-3px 3px 0px ${gigas},
-                         -6px 6px 0px #5f85c5,
-                         -9px 9px 0px ${mayaBlue};`
+export const multiple = `-2px 2px 0px ${gigas},
+                         -4px 4px 1px #5f85c5,
+                         -6px 6px 2px ${mayaBlue};`
